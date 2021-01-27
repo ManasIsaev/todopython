3c16f6dcb7f25e2fa2b320a73e6bf42b25ec5b47
Atai Isaev <atai.b.isaev@gmail.com>
2021-01-27 13:32:43 +0600

    updated all

--- todo/settings.py
+++ todo/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h(&t3goj0kt4i^7rmwmr2inx9l1)8xqqqnr3bia17au-fan0^b'
+SECRET_KEY = '&a52i2x4634q-6vqn$pr2cc8!i01%pu-h5gke97)0)6=)0e#cb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- todo/urls.py
+++ todo/urls.py
@@ -15,16 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test, second, third
+from main.views import *
 from django.conf import settings
 from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", homepage, name="home"),
     path("test/", test, name="test"),
     path("test2/", second),
-    path("test3/", third)
-]   + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) \
-    + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path("add-todo/", add_todo, name="add-todo"),
+    path("delete-todo/<id>/", delete_todo, name="delete-todo"),
+    path("mark-todo/<id>/", mark_todo, name="mark-todo"),
+    path("close-todo/<id>/", close_todo, name="close-todo"),
+]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
